693495c98f0976f1abc6d6ebda49e135722ebe84
EC2 Default User <ec2-user@ip-172-31-15-218.us-east-2.compute.internal>
2018-06-11 14:59:43 +0000

    second

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+var filters = {
+    open: function (tasks){
+        return tasks.filter(function (task){
+            return task.status === 1
+        })
+    },
+    doing: function (tasks){
+        return tasks.filter(function (task){
+            return task.status === 2
+        })
+    },
+    closed: function (tasks){
+        return tasks.filter(function (task){
+            return task.status === 3
+        })
+    }
+}
+
 new Vue({
     el: '#board',
     data: {
@@ -7,5 +25,17 @@ new Vue({
             { name: 'task 3', status: 2, assignee: '🐱', mandays: 1},
             { name: 'task 4', status: 3, assignee: '🐹', mandays: 1}
         ]   
+    },
+    computed: {
+        tasksOpen: function() {
+            return filters.open(this.tasks)
+        },
+        tasksDoing: function() {
+            return filters.doing(this.tasks)
+        },
+        tasksClosed: function() {
+            return filters.closed(this.tasks)
+        }
     }
 })
+
